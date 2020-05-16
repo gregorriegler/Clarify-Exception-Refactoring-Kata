ba8033efd34c5d512370bfeaa4a4f10604f5889c
Gregor Riegler <gregor.riegler@ebcont.com>
2020-05-16 12:24:42 +0200

    basic coverage using approvals

--- java/src/test/java/codingdojo/MessageEnricherTest.java
+++ java/src/test/java/codingdojo/MessageEnricherTest.java
@@ -1,12 +1,10 @@
 package codingdojo;
 
 import org.approvaltests.Approvals;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class MessageEnricherTest {
 
@@ -15,8 +13,60 @@ public class MessageEnricherTest {
         MessageEnricher enricher = new MessageEnricher();
         SpreadsheetWorkbook worksheet = new SpreadsheetWorkbook();
         Exception e = new RuntimeException("Terrible problem");
+
         ErrorResult actual = enricher.enrichError(worksheet, e);
-        assertEquals("Fixme", actual.getMessage());
+
+        Approvals.verify(actual);
+    }
+
+    @Test
+    void expressionParseException() {
+        MessageEnricher enricher = new MessageEnricher();
+        SpreadsheetWorkbook worksheet = new SpreadsheetWorkbook();
+        Exception e = new ExpressionParseException("Terrible problem");
+
+        ErrorResult actual = enricher.enrichError(worksheet, e);
+
+        Approvals.verify(actual);
+    }
+
+    @Test
+    void circularReferenceException() {
+        MessageEnricher enricher = new MessageEnricher();
+        SpreadsheetWorkbook worksheet = new SpreadsheetWorkbook();
+        Exception e = new RuntimeException("Circular Reference And something else");
+
+        ErrorResult actual = enricher.enrichError(worksheet, e);
+
+        Approvals.verify(actual);
+    }
+
+    @Test
+    void vLookupException() {
+        MessageEnricher enricher = new MessageEnricher();
+        SpreadsheetWorkbook worksheet = new SpreadsheetWorkbook();
+        Exception e = irrelevant_vLookup_irrelevant();
+
+        ErrorResult actual = enricher.enrichError(worksheet, e);
+
+        Approvals.verify(actual);
+    }
+
+    @Test
+    void noMatchesFoundException() {
+        MessageEnricher enricher = new MessageEnricher();
+        SpreadsheetWorkbook worksheet = new SpreadsheetWorkbook();
+        Exception e = new RuntimeException("No matches found");
+
+        ErrorResult actual = enricher.enrichError(worksheet, e);
+
+        Approvals.verify(actual);
+    }
+
+    private RuntimeException irrelevant_vLookup_irrelevant() {
+        RuntimeException exception = new RuntimeException("Object reference not set to an instance of an object");
+        exception.fillInStackTrace();
+        return exception;
     }
 
 }
